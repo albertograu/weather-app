dd9c7bee6ceb779ba56dbd7e234658970a9f0272
Alberto Grau <agrau@caudalie-usa.com>
2018-06-14 15:12:53 -0400

    ejs templating

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+// Start the Express Server
+const app = express();
+
+// Gets root route and renders index.html
+// Instead of using res.send, we use res.render when working with a templating language. 
+app.get("/", function (request, response) {
+  response.render("index");
+});
+
+app.listen(3000, function () {
+  console.log("Example app is running on port 3000!");
+});
+
+// EJS templating language
+app.set('view engine', 'ejs')
