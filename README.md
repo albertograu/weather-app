09cb652da6137f2c6a7f05b9bb0c7b820fe0dfb6
Alberto Grau <agrau@caudalie-usa.com>
2018-06-14 15:33:29 -0400

    POST response and bodyParser

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A node weather app
+
+###
+Run on localhost:3000 `./node_modules/nodemon/bin/nodemon.js server.js`
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
-// Start the Express Server
 const app = express();
+const bodyParser = require('body-parser');
 
-// Gets root route and renders index.html
+// Use CSS styles in public folder
+app.use(express.static('public'));
+// Body parser middleware (Express). By using body-parser we can make use of the req.body object.
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// EJS templating language
+app.set('view engine', 'ejs');
+
+// GET root route and renders index.html
 // Instead of using res.send, we use res.render when working with a templating language. 
 app.get("/", function (request, response) {
   response.render("index");
 });
 
+// POST
+app.post("/", function (request, response) {
+  response.render("index");
+  console.log(request.body.city);
+});
+
 app.listen(3000, function () {
   console.log("Example app is running on port 3000!");
 });
-
-// EJS templating language
-app.set('view engine', 'ejs');
-// Use CSS styles in public folder
-app.use(express.static('public'));
