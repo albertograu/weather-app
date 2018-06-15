b0bfafa27bf842385ec487dad818a5674bcbba2c
Alberto Grau <agrau@caudalie-usa.com>
2018-06-15 11:17:29 -0400

    rename to Procfile

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.post('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
